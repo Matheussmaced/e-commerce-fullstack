810848e25acff3eac97996dd7a2388ab0f6cdf98
Matheussmaced <mateusbyakuya3@gmail.com>
2026-03-30 14:25:38 -0300

    fix: force_https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+     public function boot(): void
     {
-        //
+        if (env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
     }
 }
